4c51586fa786fd8ac8532885530c8592b64db72e
usm0n8 <gazievusmon1319@gmail.com>
2026-02-07 17:53:29 +0500

    hometask completed

---
+++ hometsak/Program.cs
@@ -0,0 +1,195 @@
+﻿// 1;
+
+double sum;
+sum = Convert.ToInt32(Console.ReadLine());
+if (sum < 100)
+{
+    sum += sum * 0.05;
+}
+else if (sum <= 200)
+{
+    sum += sum * 0.07;
+}
+else
+{
+    sum += sum * 0.10;
+}
+
+Console.WriteLine(sum);
+
+
+// 2;
+
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int p = 1;
+// string o = "";
+
+// for (int i = 0; i < n; i++)
+// {
+//     int x = Convert.ToInt32(Console.ReadLine());
+
+//     p *= x;
+
+//     o += x;
+//     if (i != n - 1)
+//         o += "*";
+// }
+
+// Console.WriteLine(o + " = " + p);
+
+// 3;
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] a = new int[n];
+
+// for (int i = 0; i < n; i++)
+//     a[i] = Convert.ToInt32(Console.ReadLine());
+
+// int max = a[0], index = 0;
+
+// for (int i = 1; i < n; i++)
+// {
+//     if (a[i] > max)
+//     {
+//         max = a[i];
+//         index = i;
+//     }
+// }
+
+// Console.WriteLine(index);
+
+
+// 4;
+
+// int n = Convert.ToInt32(Console.ReadLine()); 
+// string[] arr = new string[n];
+
+// for (int i = 0; i < n; i++)
+//     arr[i] = Console.ReadLine(); 
+
+// for (int i = 0; i < n; i++)
+// {
+//     bool c = true;
+//     for (int j = 0; j < n; j++)
+//     {
+//         if (i != j && arr[i] == arr[j])
+//         {
+//             c = false;
+//             break;
+//         }
+//     }
+//     if (c)
+//         Console.Write(arr[i] + " ");
+// }
+
+
+// 5;
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] arr = new int[n];
+
+// for (int i = 0; i < n; i++) arr[i] = Convert.ToInt32(Console.ReadLine());
+
+// int min = arr[0], index = 0;
+
+// for (int i = 1; i < n; i++)
+// {
+//     if (arr[i] < min)
+//     {
+//         min = arr[i];
+//         index = i;
+//     }
+// }
+
+// Console.WriteLine(index);
+
+
+// 6;
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] arr = new int[n];
+
+// for (int i = 0; i < n; i++)
+//     arr[i] = Convert.ToInt32(Console.ReadLine());
+
+// int cnt = 0;
+
+// for (int i = 1; i < n - 1; i++)
+// {
+//     if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
+//         cnt++;
+// }
+
+// Console.WriteLine(cnt);
+
+
+// 7;
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] arr = new int[n];
+
+// for (int i = 0; i < n; i++)
+//     arr[i] = Convert.ToInt32(Console.ReadLine());
+
+// int f = 0, s = 0;
+// bool b = false;
+
+// for (int i = 0; i < n - 1; i++)
+// {
+//     if ((arr[i] > 0 && arr[i + 1] > 0) || (arr[i] < 0 && arr[i + 1] < 0))
+//     {
+//         f = arr[i];
+//         s = arr[i + 1];
+//         b = true;
+//     }
+// }
+
+// if (b) Console.WriteLine(f + " " + s);
+
+// 8;
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] arr = new int[n];
+
+// for (int i = 0; i < n; i++)
+//     arr[i] = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 0; i < n; i++)
+// {
+//     if (arr[i] % 2 != 0)
+//         Console.Write(i + " ");
+// }
+
+
+// 9;
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] arr = new int[n];
+
+// for (int i = 0; i < n; i++)
+//     arr[i] = Convert.ToInt32(Console.ReadLine());
+
+// int start = Convert.ToInt32(Console.ReadLine()); 
+// int end = Convert.ToInt32(Console.ReadLine()); 
+
+// for (int i = start; i <= end; i++)
+// {
+//     if (arr[i] % 2 != 0)
+//         Console.Write(arr[i] + " ");
+// }
+
+
+// 10;
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] a = new int[n];
+
+// for (int i = 0; i < n; i++)
+//     a[i] = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 0; i < n; i++)
+// {
+//     if (a[i] % 2 != 0)
+//         Console.Write(a[i] * a[i] + " ");
+// }
---
+++ lesson-2/Lesson-1/Program.cs
@@ -0,0 +1,118 @@
+﻿// 1
+// int max = int.MinValue;
+// for (;;)
+// {
+//     int a = Convert.ToInt32(Console.ReadLine());
+//     if(a==0)break;
+//     if(a>max)max=a;
+// }
+// System.Console.WriteLine(max);
+
+// 2
+// double a, b, sum=0;
+// a = Convert.ToInt32(Console.ReadLine());
+// b = Convert.ToInt32(Console.ReadLine());
+// for(int i=0; i<=b; i++)
+// {
+//     sum+=Math.Pow(a,i);
+// }
+// System.Console.WriteLine(sum);
+
+// 3
+// int cnt = 0;
+// int pew = -1;
+
+// for(;;)
+// {
+//     int a = Convert.ToInt32(Console.ReadLine());
+//     if(pew == 0 && a == 0)
+//     {
+//         break;
+//     }
+//     cnt+= a;
+
+//     pew = a;
+// }
+// System.Console.WriteLine(cnt);
+
+
+// 4
+// int num = Convert.ToInt32(Console.ReadLine());
+// int [] arr = new int [num];
+// for(int i=0; i<num; i++)
+// {
+//     arr[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// bool f = false;
+// for(int i=0; i<num-1; i++)
+// {
+//     if(arr[i]>0 && arr[i+1]>0 || arr[i]<0 && arr[i+1]<0){f=true; break;}
+// }
+// if(f) System.Console.WriteLine("YES");
+// else System.Console.WriteLine("NO");
+ 
+
+// 6
+// int a = Convert.ToInt32(Console.ReadLine());
+// int[] arr = new int[a];
+
+// for (int i = 0; i < a; i++)
+// {
+//     arr[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+// int min = arr[0];   
+
+// for (int i = 1; i < a; i++)
+// {
+//     if (arr[i] < min)
+//     {
+//         min = arr[i];
+//     }
+// }
+
+// Console.WriteLine(min);
+
+// 7
+// int sum = 0;
+// int cnt = 0;
+
+// while (true)
+// {
+//     Console.WriteLine("Give a number:");
+//     Console.Write("> ");
+
+//     int i = Convert.ToInt32(Console.ReadLine());
+
+//     if (i == 0)
+//         break; 
+
+//     sum += i;
+//     cnt++;
+// }
+
+// Console.WriteLine("Sum of numbers: " + sum);
+// Console.WriteLine("Count of numbers: " + cnt);
+
+// 8
+// int a, c, p=1, sum=0;
+// a = Convert.ToInt32(Console.ReadLine());
+
+// for(int i = a;i > 0; i/=10)
+// {
+//     c = i % 10;
+//     sum += c;
+//     p *= c;
+// }
+// System.Console.WriteLine(sum);
+// System.Console.WriteLine(p);
+
+// 9
+// int a, rev = 0;
+// a = Convert.ToInt32(Console.ReadLine());
+
+// for(int i = a;i > 0;i/=10)
+// {
+//     rev = rev * 10 + i % 10;
+// }
+// System.Console.WriteLine(rev);
